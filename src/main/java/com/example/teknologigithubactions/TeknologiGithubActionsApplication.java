7756799dec5bf58aae4564e39b70bb6c63cb24e8
mattias <mattiaslarsen@hotmail.com>
2026-03-03 16:18:07 +0100

    Initial commit

---
+++ src/main/java/com/example/teknologigithubactions/TeknologiGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknologigithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeknologiGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teknologigithubactions/TeknologiGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknologigithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
