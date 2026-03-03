ee98e7436a33d7e770d9314c7ea68b8bd5b2a622
mattias <mattiaslarsen@hotmail.com>
2026-03-03 16:54:08 +0100

    Add WelcomeController and welcome view

--- src/main/java/com/example/teknologigithubactions/TeknologiGithubActionsApplication.java
+++ src/main/java/com/example/teknologigithubactions/TeknologiGithubActionsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeknologiGithubActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeknologiGithubActionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiGithubActionsApplication.class, args);
+    }
 
 }
